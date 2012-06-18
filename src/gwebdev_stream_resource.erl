d404de251f970ebbb9089c53aa233fceff2fea0b
Jeff Bell <jeff@j2-c2.com>
2012-06-17 21:14:03 -0400

    Added streambody example resource

---
+++ src/gwebdev_stream_resource.erl
@@ -0,0 +1,30 @@
+-module(gwebdev_stream_resource).
+-export([init/1, allowed_methods/2, process_post/2]).
+
+-include_lib("webmachine/include/webmachine.hrl").
+
+init([]) -> {ok, undefined}.
+
+allowed_methods(ReqData, State) -> {['POST'], ReqData, State}.
+
+process_post(ReqData, State) ->
+    Body = get_streamed_body(wrq:stream_req_body(ReqData, 3), []),
+    {true, wrq:set_resp_body({stream, send_streamed_body(Body,4)},ReqData), State}.
+
+send_streamed_body(Body, Max) ->
+    HunkLen=8*Max,
+    case Body of
+        <<Hunk:HunkLen/bitstring, Rest/binary>> ->
+            io:format("SENT ~p~n",[Hunk]),
+            {Hunk, fun() -> send_streamed_body(Rest,Max) end};
+        _ ->
+            io:format("SENT ~p~n",[Body]),
+            {Body, done}
+    end.
+
+get_streamed_body({Hunk,done},Acc) ->
+    io:format("RECEIVED ~p~n",[Hunk]),
+    iolist_to_binary(lists:reverse([Hunk|Acc]));
+get_streamed_body({Hunk,Next},Acc) ->
+    io:format("RECEIVED ~p~n",[Hunk]),
+    get_streamed_body(Next(),[Hunk|Acc]).
